a9ab0e0989d22ddb564268ff2aa4d514289ddffd
borfus <mrpeteyb@gmail.com>
2021-12-07 10:17:33 -0600

    added bool for day 7 parameter to shorten code

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -4,11 +4,11 @@ use std::i32;
 pub fn run() {
     let data = fs::read_to_string("input/day7.txt").unwrap();
 
-    println!("Day 7 - Part 1: {}", least_fuel(&data));
-    println!("Day 7 - Part 2: {}", least_fuel_increasing(&data));
+    println!("Day 7 - Part 1: {}", least_fuel(&data, false));
+    println!("Day 7 - Part 2: {}", least_fuel(&data, true));
 }
 
-fn least_fuel(data: &String) -> i32 {
+fn least_fuel(data: &String, increasing: bool) -> i32 {
     let crabs: Vec<i32> = data.split_whitespace().collect::<String>().split(',').collect::<Vec<&str>>().iter().map(|n| n.parse::<i32>().unwrap()).collect();
 
     let (mut min, mut max) = (i32::MAX, i32::MIN);
@@ -27,37 +27,11 @@ fn least_fuel(data: &String) -> i32 {
     for n in min..=max {
         let mut new_cost = 0;
         for i in 0..crabs.len() {
-            new_cost += (crabs[i] - n).abs();
-        }
-
-        if new_cost < total_cost {
-            total_cost = new_cost;
-        }
-    }
-
-    total_cost
-}
-
-fn least_fuel_increasing(data: &String) -> i32 {
-    let crabs: Vec<i32> = data.split_whitespace().collect::<String>().split(',').collect::<Vec<&str>>().iter().map(|n| n.parse::<i32>().unwrap()).collect();
-
-    let (mut min, mut max) = (i32::MAX, i32::MIN);
-    for crab in &crabs {
-        if *crab > max {
-            max = *crab;
-        }
-
-        if *crab < min {
-            min = *crab;
-        }
-    }
-
-    let mut total_cost = i32::MAX;
-
-    for n in min..=max {
-        let mut new_cost = 0;
-        for i in 0..crabs.len() {
-            new_cost += (crabs[i] - n).abs() * ((crabs[i] - n).abs() + 1) / 2;
+            if !increasing {
+                new_cost += (crabs[i] - n).abs();
+            } else {
+                new_cost += (crabs[i] - n).abs() * ((crabs[i] - n).abs() + 1) / 2;
+            }
         }
 
         if new_cost < total_cost {
@@ -74,7 +48,7 @@ mod tests {
     fn part1() {
         let data = String::from("16,1,2,0,4,2,7,1,2,14");
         let expected = 37;
-        let actual = super::least_fuel(&data);
+        let actual = super::least_fuel(&data, false);
         assert_eq!(expected, actual);
     }
 
@@ -82,7 +56,7 @@ mod tests {
     fn part2() {
         let data = String::from("16,1,2,0,4,2,7,1,2,14");
         let expected = 168;
-        let actual = super::least_fuel_increasing(&data);
+        let actual = super::least_fuel(&data, true);
         assert_eq!(expected, actual);
     }
 }
