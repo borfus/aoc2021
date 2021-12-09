3f809c6e3a05c0fdda8756a424b2e62bd7355fd3
borfus <mrpeteyb@gmail.com>
2021-12-09 13:28:54 -0600

    completed day 9

--- src/days/day9.rs
+++ src/days/day9.rs
@@ -16,7 +16,7 @@ pub fn run() {
 fn height_map_sum(data: &Vec<String>, find_basin: bool) -> i32 {
     let mut result = 0;
     let mut height_map: Vec<Vec<i32>> = vec![];
-    let mut basins: Vec<i32> = vec![];
+    let mut basin_totals: Vec<i32> = vec![];
 
     for line in data {
         let mut row: Vec<i32> = vec![];
@@ -32,35 +32,41 @@ fn height_map_sum(data: &Vec<String>, find_basin: bool) -> i32 {
                 if !find_basin {
                     result += height_map[x][y] + 1;
                 } else {
-                    let current_basin = get_basin(&height_map, x, y);
+                    basin_totals.push(get_basin_sum(&height_map, x, y));
                 }
             }
         }
+    }
+
+    if find_basin {
+        basin_totals.sort();
+        let len = basin_totals.len();
+        result = basin_totals[len-1] * basin_totals[len-2] * basin_totals[len-3];
     }
 
     result
 }
 
 fn is_low_point(current_height: i32, height_map: &Vec<Vec<i32>>, x: usize, y: usize) -> bool {
-    let mut surrounding: Vec<i32> = vec![];
+    let mut neighbors: Vec<i32> = vec![];
 
     if x != 0 {
-        surrounding.push(height_map[x-1][y]);
+        neighbors.push(height_map[x-1][y]);
     }
 
     if x != height_map.len() - 1 {
-        surrounding.push(height_map[x+1][y]);
+        neighbors.push(height_map[x+1][y]);
     }
 
     if y != 0 {
-        surrounding.push(height_map[x][y-1]);
+        neighbors.push(height_map[x][y-1]);
     }
 
     if y != height_map[x].len() - 1 {
-        surrounding.push(height_map[x][y+1]);
+        neighbors.push(height_map[x][y+1]);
     }
 
-    for n in surrounding {
+    for n in neighbors {
         if current_height >= n {
             return false;
         }
@@ -69,10 +75,45 @@ fn is_low_point(current_height: i32, height_map: &Vec<Vec<i32>>, x: usize, y: us
     true
 }
 
-fn get_basin(height_map: &Vec<Vec<i32>>, x: usize, y: usize) -> Vec<i32> {
-    let mut result: Vec<i32> = vec![];
+fn get_basin_sum(height_map: &Vec<Vec<i32>>, x: usize, y: usize) -> i32 {
+    let mut added_coords: Vec<(usize, usize)> = vec![];
+    added_coords.push((x, y));
 
-    result
+    let mut i = 0;
+    while i < added_coords.len() {
+        let x = added_coords[i].0;
+        let y = added_coords[i].1;
+        if x != 0 {
+            let coords = (x-1, y);
+            if !added_coords.contains(&coords) && height_map[coords.0][coords.1] != 9 {
+                added_coords.push(coords);
+            }
+        }
+
+        if x != height_map.len() - 1 {
+            let coords = (x+1, y);
+            if !added_coords.contains(&coords) && height_map[coords.0][coords.1] != 9 {
+                added_coords.push(coords);
+            }
+        }
+
+        if y != 0 {
+            let coords = (x, y-1);
+            if !added_coords.contains(&coords) && height_map[coords.0][coords.1] != 9 {
+                added_coords.push(coords);
+            }
+        }
+
+        if y != height_map[x].len() - 1 {
+            let coords = (x, y+1);
+            if !added_coords.contains(&coords) && height_map[coords.0][coords.1] != 9 {
+                added_coords.push(coords);
+            }
+        }
+        i += 1;
+    }
+
+    added_coords.len() as i32
 }
 
 #[cfg(test)]
